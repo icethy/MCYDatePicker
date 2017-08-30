6c3948341393a3082cef3faf5b6db553792004d1
machunyan <machunyan@yuchuangdemacbook-air.local>
2017-08-30 09:21:31 +0800

    Initial Commit

---
+++ MCYDatePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCYDatePicker
+//
+//  Created by machunyan on 2017/8/30.
+//  Copyright © 2017年 马春燕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MCYDatePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MCYDatePicker
+//
+//  Created by machunyan on 2017/8/30.
+//  Copyright © 2017年 马春燕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
